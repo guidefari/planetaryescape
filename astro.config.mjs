ab88c3ad6ac407edf5a7f5cb34769ba152bb734f
Guide Fari <guideg6@gmail.com>
2024-10-01 08:11:18 +0200

    landing page wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-   vite: {
-    plugins: [tailwindcss()],
-  },
-  // add yur domain name here
-  site: 'https://lexingtonthemes.com',
-  compressHTML: true,
-  integrations: [sitemap()]
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	// add yur domain name here
+	site: "https://planetaryescape.xyz",
+	compressHTML: true,
+	integrations: [sitemap()],
 });
