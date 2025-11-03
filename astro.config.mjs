b217d022691dfe378db0777b848eea05b8013bbb
Guide Fari <guideg6@gmail.com>
2025-11-03 18:00:46 +0200

    sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
-	// add yur domain name here
-	site: "https://planetaryescape.xyz",
-	compressHTML: true,
-	integrations: [sitemap()],
+
+  // add yur domain name here
+  site: "https://planetaryescape.xyz",
+
+  compressHTML: true,
+  integrations: [sitemap()],
+  adapter: vercel(),
 });
